966970cf14142323f2ea49a2e614869c9a07e315
YanniAlshoufiHTL <y.al_shoufi@HTBLALeonding.onmicrosoft.com>
2024-10-04 19:07:40 +0200

    Add exercise with template to copy

---
+++ Looping/Template/Program.cs
@@ -0,0 +1,59 @@
+﻿// This is a template project. Copy it and work with the copy.
+// Before starting with the exercise, DEACTIVATE ANY AI TOOLS.
+// You are allowed to use snippets.
+
+// You have to reach the goal time at least three times.
+// Keep copying the original project every time.
+// Do this exercise at different days.
+
+// Time of trial: xx min.
+// Goal Time:      5 min.
+
+// Given the following array:
+string[] names = ["Yanni", "Rudradeep", "Samuel", "Britta", "Amelie", "Alex"];
+
+#region USING NORMAL FOR LOOPS
+
+// Print out all names
+
+Console.WriteLine();
+// Print out every second name
+
+Console.WriteLine();
+// Print out every third name
+
+Console.WriteLine();
+// Print out all names that start with an 'A'
+
+Console.WriteLine();
+// Print out all names that do not start with an 'A'
+
+Console.WriteLine();
+// Print out all names again
+
+Console.WriteLine();
+// And again
+
+Console.WriteLine();
+// And again
+
+#endregion
+
+#region USING FOR EACH LOOPS
+
+Console.WriteLine();
+// Print out all names
+
+Console.WriteLine();
+// Print out all names that start with an 'A'
+
+Console.WriteLine();
+// Print out all names again
+
+Console.WriteLine();
+// Print out all names that do not start with an 'A'
+
+Console.WriteLine();
+// Print out all names and their indexes, use a counting variable
+
+#endregion
